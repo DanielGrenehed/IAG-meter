e7f3fa453ff135ed69cbdc8dd7740a16f9d09564
Daniel Grenehed <daniel.grenehed97@gmail.com>
2020-09-24 10:55:56 +0200

    Update insuline_injection_logger.py

--- python/insuline_injection_logger.py
+++ python/insuline_injection_logger.py
@@ -2,16 +2,11 @@
 
 """
 Handle insuline injection data
-
-
     example arguments:
-
     ->  H 7e
         Humalog, 7 units, now
-
     ->  L 16e 20.39
         Lantus, 16 units, today 20:39
-
 """
 
 class IILogger(object):
@@ -20,10 +15,13 @@ class IILogger(object):
         Insuline Injection Logger
     """
 
-    def __init__(self, arg): # change arg to storage abstraction
+    def __init__(self, storage): # change arg to storage abstraction
         super(IILogger, self).__init__()
-        self.arg = arg
+        self.storage = storage
 
+    """
+        Store data in storage
+    """
     def log_intake(self, ammount, insuline_type, time):
         # store data.
         print("a", str(ammount), " t", insuline_type, " d", time)
@@ -42,7 +40,9 @@ class IIL_Wrapper(object):
         self.get_time = get_time
         self.__construct_function_map()
 
-
+    """
+        Create a alias map for functions
+    """
     def __construct_function_map(self):
         self.function_map = {
             "IH": [1, self.log_humalog_now], # IH: Humalog, units (now)
@@ -51,21 +51,36 @@ class IIL_Wrapper(object):
             "Il": [2, self.log_lantus], # Il: Lantus, units, time
         }
 
+    """
+        Return the function_map
+    """
     def get_function_map(self):
         return self.function_map
 
+    """
+        Log insuline injection of type Humalog
+    """
     def log_humalog(self, param):
         ammount, time = param
         self.logger.log_intake(int(ammount), "humalog", time)
 
+    """
+        Log insuline injection of type Humalog with time set to current time
+    """
     def log_humalog_now(self, param):
         ammount, = param
         self.log_humalog([ammount, self.get_time()])
 
+    """
+        Log insuline injection of type Lantus
+    """
     def log_lantus(self, param):
         ammount, time = param
         self.logger.log_intake(int(ammount), "lantus", time)
 
+    """
+        Log insuline injection of type Lantus with time set to current time
+    """
     def log_lantus_now(self, param):
         ammount, = param
         self.log_lantus([ammount, self.get_time()])
